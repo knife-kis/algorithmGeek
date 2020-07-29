468689726dfb54b51d7d5f59be67d7518f065173
knife-kis <36123852+knife-kis@users.noreply.github.com>
2020-07-29 22:09:08 +0700

    first

---
+++ algorithm/src/com/tarnovskiy/Main.java
@@ -0,0 +1,13 @@
+package com.tarnovskiy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> listStr = new ArrayList<>();
+        listStr.add("123 asdf");
+        listStr.add("!");
+    }
+}
