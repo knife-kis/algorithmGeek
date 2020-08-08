0a0630b34f3e27a6abe198c7d2eb4158b534e10a
knife-kis <36123852+knife-kis@users.noreply.github.com>
2020-08-08 10:06:49 +0700

    fix lesson5

---
+++ algorithm/src/com/tarnovskiy/lesson2/MySortedArrayList.java
@@ -0,0 +1,58 @@
+package com.tarnovskiy.lesson2;
+
+public class MySortedArrayList<T extends Comparable<T>> extends MyArrayList<T> {
+
+    @Override
+    public void add(T item) {
+        int i = 0;
+        while (i < size() && item.compareTo(get(i)) >= 0) {
+            i++;
+        }
+        super.add(i, item);
+    }
+
+    @Override
+    public void add(int index, T item) {
+        add(item);
+    }
+
+    @Deprecated
+    @Override
+    public void set(int index, T item) {
+        throw new UnsupportedOperationException("Данный метод в этм классе нельзя использовать");
+    }
+
+    public int binaryFind(T item) {
+        int lo = 0;
+        int hi = size() - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;//  (lo + hi)/2
+            if (item.compareTo(get(mid)) < 0) {
+                hi = mid - 1;
+            } else if (item.compareTo(get(mid)) > 0) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    public int recBinaryFind(T item) {
+        return recBinaryFind(item, 0, size() - 1);
+    }
+
+    private int recBinaryFind(T item, int lo, int hi) {
+        int mid = lo + (hi - lo) / 2;
+        if (item.compareTo(get(mid)) == 0) {
+            return mid;
+        } else if (lo == hi) {
+            return -1;
+        } else if (item.compareTo(get(mid)) < 0) {
+            return recBinaryFind(item, lo, mid);
+        } else {
+            return recBinaryFind(item, mid + 1, hi);
+        }
+    }
+}
