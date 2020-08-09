4cc30b4062142a913483992a0c44dd699c21157a
knife-kis <36123852+knife-kis@users.noreply.github.com>
2020-08-09 20:09:00 +0700

    hw06

--- algorithm/src/com/tarnovskiy/lesson6/Main.java
+++ algorithm/src/com/tarnovskiy/lesson6/Main.java
@@ -1,28 +1,53 @@
 package com.tarnovskiy.lesson6;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
-//        creator(20,6);
 
-        MyTreeMap<Integer, String> map = new MyTreeMap<>();
 
-        map.put(5,"five");
-        map.put(1,"one");
-        map.put(3,"three");
-        map.put(2,"two");
+        creator(20,6);
 
-        System.out.println(map);
-        System.out.println(map.get(2));
-        map.put(2,"ttt");
-        System.out.println(map);
-
-        map.deleteMin();
-        System.out.println(map);
+//        MyTreeMap<Integer, String> map = new MyTreeMap<>();
+//
+//        map.put(5,"five");
+//        map.put(1,"one");
+//        map.put(3,"three");
+//        map.put(2,"two");
+//
+//        System.out.println(map);
+//        System.out.println(map.get(2));
+//        map.put(2,"ttt");
+//        System.out.println(map);
+//
+//        map.deleteMin();
+//        System.out.println(map);
 
     }
 
-//    private static void creator(int countTree, int depthTree) {
-//
-//    }
+    private static void creator(int countTree, int depthTree) {
+
+        int balanced = 0;
+        for (int i = 0; i < countTree; i++) {
+            MyTreeMap<Integer, Integer> map = new MyTreeMap<>();
+            while (map.depth() < depthTree){
+                map.put(insertNumber(100),0);
+            }
+            if (map.isBalanced()){
+                balanced++;
+            }
+        }
+
+        System.out.println("from " + countTree + " MyTreeMap balanced is " + balanced + "\r\nnot balanced is " + (countTree - balanced) + " trees");
+    }
+
+    private static Integer insertNumber(int i) {
+        Random random = new Random();
+        if (random.nextInt(i) % 2 == 0){
+            return random.nextInt(i);
+        } else{
+            return -random.nextInt(i);
+        }
+    }
 }
