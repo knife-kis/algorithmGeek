e66a47d51c646c0d298572b0fdcc27b5ff7abff1
knife-kis <36123852+knife-kis@users.noreply.github.com>
2020-08-09 19:37:53 +0700

    les06

---
+++ algorithm/src/com/tarnovskiy/lesson6/Main.java
@@ -0,0 +1,28 @@
+package com.tarnovskiy.lesson6;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        creator(20,6);
+
+        MyTreeMap<Integer, String> map = new MyTreeMap<>();
+
+        map.put(5,"five");
+        map.put(1,"one");
+        map.put(3,"three");
+        map.put(2,"two");
+
+        System.out.println(map);
+        System.out.println(map.get(2));
+        map.put(2,"ttt");
+        System.out.println(map);
+
+        map.deleteMin();
+        System.out.println(map);
+
+    }
+
+//    private static void creator(int countTree, int depthTree) {
+//
+//    }
+}
